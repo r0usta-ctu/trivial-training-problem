dc07d2a8a1bda9a091e40d0822929efad2c1da1d
r0usta-ctu <levderos@cvut.cz>
2025-09-29 15:34:51 +0200

    Replace `ProjectRootManager` JDK configuration and refactor `Main.java` for better input handling

--- alg/Main.java
+++ alg/Main.java
@@ -1,40 +1,48 @@
-int[] numbers;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-boolean tryLoadData() {
-    StringTokenizer tokenizer = new StringTokenizer(IO.readln());
+public class Main {
+    public static int[] numbers;
 
-    int tokensCount = tokenizer.countTokens();
-    if (tokensCount < 2) return false;
-
-    numbers = new int[tokensCount];
-    for (int i = 0; i < tokensCount; i++) {
-        numbers[i] = Integer.parseInt(tokenizer.nextToken());
-    }
-
-    return true;
-}
-
-void main() {
-    if (!tryLoadData())
-        return;
-
-    Arrays.sort(numbers);
-    StringBuilder builder = new StringBuilder();
-
-    int lastNum = numbers[0];
-    boolean hasDuplicates = false;
-    for (int i = 1; i < numbers.length; i++) {
-        if (lastNum == numbers[i]) {
-            if(!hasDuplicates) {
-                builder.append(String.format(" %d", numbers[i]));
-                hasDuplicates = true;
-            }
+    private static boolean tryLoadData() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int tokensCount = tokenizer.countTokens();
+        if (tokensCount < 2) {
+            return false;
         } else {
-            hasDuplicates = false;
-        }
+            numbers = new int[tokensCount];
+            for(int i = 0; i < tokensCount; ++i) {
+                numbers[i] = Integer.parseInt(tokenizer.nextToken());
+            }
 
-        lastNum = numbers[i];
+            return true;
+        }
     }
 
-    IO.println(builder.toString());
+    public static void main(String[] args) throws IOException {
+        if (tryLoadData()) {
+            Arrays.sort(numbers);
+            StringBuilder builder = new StringBuilder();
+            int lastNum = numbers[0];
+            boolean hasDuplicates = false;
+            for(int i = 1; i < numbers.length; ++i) {
+                if (lastNum == numbers[i]) {
+                    if (!hasDuplicates) {
+                        builder.append(String.format(" %d", numbers[i]));
+                        hasDuplicates = true;
+                    }
+                } else {
+                    hasDuplicates = false;
+                }
+
+                lastNum = numbers[i];
+            }
+
+            System.out.println(builder);
+        }
+    }
 }
